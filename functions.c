20eff8a9883dfd194402b666ec028323ba1b8253
Nkuli0109 <n.sikali@gmail.com>
2023-11-14 07:18:57 -0800

    edited Printf files

--- functions.c
+++ functions.c
@@ -157,8 +157,7 @@ int print_int(va_list types, char buffer[],
  * @size: Size specifier
  * Return: Numbers of char printed
  */
-int print_binary(va_list types, char buffer[],
-		int flags, int width, int precision, int size)
+int print_binary(va_list types, char buffer[], int flags, int width, int precision, int size)
 {
 	unsigned int n, m, i, sum;
 	unsigned int a[32];
@@ -168,7 +167,7 @@ int print_binary(va_list types, char buffer[],
 	UNUSED(flags);
 	UNUSED(width);
 	UNUSED(precision);
-	UNUSED(size)
+	UNUSED(size);
 
 	n = va_arg(types, unsigned int);
 	m = 2147483648; /* (2 ^ 31) */
@@ -176,7 +175,7 @@ int print_binary(va_list types, char buffer[],
 	for (i = 1; i < 32; i++)
 	{
 		m /= 2;
-		a[i] = (n / m) % 2,
+		a[i] = (n / m) % 2;
 	}
 	for (i = 0, sum = 0, count = 0; i < 32; i++)
 	{
